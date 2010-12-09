9ed24fe8019edabeedab31b1735ad32cc66d92ab
gpminsuk <gpminsuk@cf0a6d36-63fd-11de-b47b-210fc227597e>
2010-12-09 16:37:56 +0000

    사운드

--- trunk/Projects/UniqueCore/Include/CWaveIODriver.h
+++ trunk/Projects/UniqueCore/Include/CWaveIODriver.h
@@ -1,8 +1,9 @@
 #pragma once
 
+#include "TDataTypes.h"
 #include "BSoundDriver.h"
 
-//#pragma comment(lib, "WaveIO.lib")
+#pragma comment(lib, "WaveIO.lib")
 
 class CWaveIODriver : public BSoundDriver
 {
@@ -14,8 +15,8 @@ public:
 	virtual bool DestroySoundDriver();
 
 	virtual bool LoadSound(TString Filename);
-	virtual bool PlayWAVSound();
+	virtual bool PlayWAVSound(int Index);
 
 	class CWavePlayer* Player;
-	class CWaveReader* Reader;
+	TArray<class CWaveReader*> Readers;
 };
